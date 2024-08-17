684187f16eaae30c388184b4a7b418894db6658b
Orr Kislev <orrkislev@gmail.com>
2024-08-17 19:33:27 +0300

    much simpler setup (works for vscode)

--- src/sketch.js
+++ src/sketch.js
@@ -1,12 +1,20 @@
-import { testThing } from "./test.js";
+import { testThing } from "./test";
 
 function setup() {
   createCanvas(400, 400);
+
+  const x = 50
+  x = 20 // should throw an error
 }
 
 function draw() {
   background(220); 
   testThing()
+
+  circle(200,200,50) // good circle
+
+  circel(200,200,50) // typo in function name
+  circle(200,200) // missing radius
 }
 
 window.setup = setup;
